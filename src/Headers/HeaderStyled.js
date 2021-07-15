486626d3e4ab3bc665d92824c0f4c48690cc3631
joice-silva <joice.jss2016@gmail.com>
2021-07-15 16:06:17 -0300

    Header-Joice

---
+++ src/Headers/HeaderStyled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const HeaderDesign = styled.div`
+    width: 100%;
+    
+`
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
-import { CarPage } from "../Pages/CarPage";
-import { DetailsPage } from "../Pages/DetailsPage";
+import  HeaderTest  from "../Headers/HeaderTest";
 import { HomePage } from "../Pages/HomePage";
+import { DetailsPage } from "../Pages/DetailsPage";
+import { CarPage } from "../Pages/CarPage";
 
 export const Router = () =>{
     return(
         <BrowserRouter>
+         <HeaderTest/>
             <Switch>
-                <Route exact path ='/' component={HomePage} />
+            <Route exact path="/">
+          <HomePage />
+        </Route>
                 <Route path ='/details' component={DetailsPage} />
                 <Route path ='/carPage' exact component={CarPage} />
             </Switch>
